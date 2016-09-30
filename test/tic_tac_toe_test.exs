58ee953153b59b4c7d835fe1d6a95db104922ba5
Daisy Molving <daisymolving@gmail.com>
2016-09-30 11:37:30 +0100

    tests that a new board is empty upon creation

--- test/tic_tac_toe_test.exs
+++ test/tic_tac_toe_test.exs
@@ -5,4 +5,10 @@ defmodule TicTacToeTest do
     assert Enum.count(TicTacToe.Board.create_new_board) == 9
   end
 
+  test "a new board is empty" do
+    assert Enum.all?(TicTacToe.Board.create_new_board, fn(space) ->
+      String.to_integer(space)
+    end) == true
+  end
+
 end
