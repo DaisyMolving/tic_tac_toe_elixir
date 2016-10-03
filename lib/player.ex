3f90a92039019c50c2ac95d35106e623f0b7c26f
Daisy Molving <daisymolving@gmail.com>
2016-10-03 14:18:57 +0100

    creates player with name and marker as struct

--- lib/player.ex
+++ lib/player.ex
@@ -1,3 +1,13 @@
 defmodule TicTacToe.Player do
 
+  defstruct name: "Player", marker: "x"
+
+  def name(given_name) do
+    String.capitalize(given_name)
+  end
+
+  def marker(given_marker) do
+    given_marker
+  end
+
 end
--- test/player_test.exs
+++ test/player_test.exs
@@ -1,1 +1,19 @@
+defmodule TicTacToeTest.Player do
+  use ExUnit.Case
 
+  test "player has a default name and marker" do
+    new_player = %TicTacToe.Player{}
+    assert new_player.name == "Player"
+    assert new_player.marker == "x"
+  end
+
+  test "player has a name that is capitalized" do
+    assert TicTacToe.Player.name("Daisy") == "Daisy"
+    assert TicTacToe.Player.name("daisy") == "Daisy"
+  end
+
+  test "player has a marker" do
+    assert TicTacToe.Player.marker("x") == "x"
+  end
+
+end
