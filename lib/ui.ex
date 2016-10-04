e67dba7cd7f76a016bf64dc32c66d4573768f6ed
Daisy Molving <daisymolving@gmail.com>
2016-10-04 16:22:53 +0100

    removes printer_receiver and instead ui prints and receives. modifies tests and functions to suit this functionality

--- lib/ui.ex
+++ lib/ui.ex
@@ -1,53 +1,35 @@
 defmodule TicTacToe.UI do
-
-  @user_input_requests [
-    {:start_or_quit, &TicTacToe.UI.welcome_introduction/0},
-    {:name_input, &TicTacToe.UI.request_set_player_name/1},
-    {:marker_input, &TicTacToe.UI.request_set_player_marker/1},
-    {:player_turn, &TicTacToe.UI.turn_message/1}
-  ]
-
-  @validation_matches [
-    {:start_or_quit, ~r/([^q, Q])/},
-    {:name_input, ~r/(\w+)/},
-    {:marker_input, ~r/(\D)/},
-    {:player_turn, ~r/([1-9])/}
-  ]
-
-  @invalid_input_messages [
-    {:start_or_quit, :end_game},
-    {:name_input, &TicTacToe.UI.invalid_name/1},
-    {:marker_input, &TicTacToe.UI.invalid_marker/1},
-    {:player_turn, &TicTacToe.UI.invalid_turn/1}
-  ]
-
   
- def welcome_introduction do
-    "Welcome to TicTacToe! This is a two player strategy game. Press q to quit, or any other key to continue: "
+  def welcome_introduction do
+    get_stripped_input("Welcome to TicTacToe! This is a two player strategy game. Press q to quit, or any other key to continue: ")
+  end
+
+  def farewell_message do
+    IO.puts("Goodbye")
   end
 
   def request_set_player_name(player_number) do
-    "Player #{player_number}, please input your name: "
+    get_stripped_input("Player #{player_number}, please input your name: ")
   end
 
   def request_set_player_marker(player_name) do
-    "Thank you #{player_name}, now please choose a marker (this can be any single symbol that is not a number): "
+    get_stripped_input("Thank you #{player_name}, now please choose a marker (this can be any single symbol that is not a number): ")
   end
   
   def invalid_name(player_number) do
-    "Sorry player #{player_number}, that is not a valid player name. Please choose a single word with no numbers: "
+    get_stripped_input("Sorry player #{player_number}, that is not a valid player name. Please choose a single word with no numbers: ")
   end
 
   def invalid_marker(player_name) do
-    "Sorry #{player_name}, that is not a valid marker. Please choose a single symbol that is NOT a number: "
+    get_stripped_input("Sorry #{player_name}, that is not a valid marker. Please choose a single symbol that is NOT a number: ")
   end
 
   def invalid_turn(player_name) do
-    "Sorry #{player_name}, that is not a valid turn. Please choose a number from 1 - 9 that matches a position that is not already taken: "
+    get_stripped_input("Sorry #{player_name}, that is not a valid turn. Please choose a number from 1 - 9 that matches a position that is not already taken: ")
   end
 
   def turn_message(player_name) do
-    "It's #{player_name}'s turn, input the number of the position that you would like to mark: "
+    get_stripped_input("It's #{player_name}'s turn, input the number of the position that you would like to mark: ")
   end
 
   def display_board(current_board) do
@@ -56,18 +38,18 @@ defmodule TicTacToe.UI do
       Enum.join(row, " ")
     end)
     |> Enum.join("\n")
-  end
-
-  def invalid_input do
-    "That is an invalid input, please try again: "
+    |> IO.puts
   end
 
   def draw_message do
-    "It's a draw!"
+    IO.puts("It's a draw!")
   end
 
   def congratulate_winner(winning_player) do
-    "#{winning_player} won! Congratulations!"
+    IO.puts("#{winning_player} won! Congratulations!")
   end
 
+  defp get_stripped_input(output_message) do
+    String.strip(IO.gets(output_message))
+  end
 end
--- test/ui_test.exs
+++ test/ui_test.exs
@@ -1,49 +1,71 @@
 defmodule TicTacToeTest.UI do
   use ExUnit.Case
+  import ExUnit.CaptureIO
   
   test "greets users and introduces game" do
-    assert TicTacToe.UI.welcome_introduction =~ "Welcome to TicTacToe! This is a two player strategy game"
+    assert capture_io("y", fn ->
+      TicTacToe.UI.welcome_introduction 
+    end) =~ "Welcome to TicTacToe! This is a two player strategy game"
   end
 
+  test "prints farewell message" do
+    assert capture_io(fn ->
+      TicTacToe.UI.farewell_message
+    end) =~ "Goodbye"
+  end
   test "asks each player to set their name" do
-    assert TicTacToe.UI.request_set_player_name("1") =~ "Player 1, please input your name: "
+    assert capture_io("Daisy", fn ->
+      TicTacToe.UI.request_set_player_name("1")
+    end) =~ "Player 1, please input your name: "
   end
 
   test "returns invalid input message for invalid player name" do
-    assert TicTacToe.UI.invalid_name("1") =~ "1, that is not a valid player name"
+    assert capture_io("Barry", fn ->
+      TicTacToe.UI.invalid_name("1") 
+    end) =~ "1, that is not a valid player name"
   end
 
   test "asks each player to set their marker" do
-    assert TicTacToe.UI.request_set_player_marker("Barry") =~ "Thank you Barry, now please choose a marker"
+    assert capture_io("x", fn ->
+      TicTacToe.UI.request_set_player_marker("Barry") 
+    end) =~ "Thank you Barry, now please choose a marker"
   end
 
   test "returns invalid input message for invalid marker" do
-    assert TicTacToe.UI.invalid_marker("Barry") =~ "Barry, that is not a valid marker"
+    assert capture_io("&", fn ->
+      TicTacToe.UI.invalid_marker("Barry") 
+    end) =~ "Barry, that is not a valid marker"
   end
 
   test "tells the users whose turn it is" do
-    assert TicTacToe.UI.turn_message("Barry") =~ "It's Barry's turn, input the number of the position that you would like to mark: "
+    assert capture_io("9", fn ->
+      TicTacToe.UI.turn_message("Barry") 
+    end) =~ "It's Barry's turn, input the number of the position that you would like to mark: "
   end
 
   test "returns invalid input message for invalid turn" do
-    assert TicTacToe.UI.invalid_turn("Barry") =~ "Barry, that is not a valid turn"
+    assert capture_io("12", fn ->
+      TicTacToe.UI.invalid_turn("Barry") 
+    end) =~ "Barry, that is not a valid turn"
   end
 
   test "displays current board" do
-    assert TicTacToe.UI.display_board(["1", "2", "3",
+    assert capture_io(fn ->
+      TicTacToe.UI.display_board(["1", "2", "3",
       "4", "5", "6",
-      "7", "8", "9"]) == "1 2 3\n4 5 6\n7 8 9"
-  end
-
-  test "returns invalid message for invalid input" do
-    assert TicTacToe.UI.invalid_input =~ "That is an invalid input, please try again: "
+      "7", "8", "9"]) 
+    end) =~ "1 2 3\n4 5 6\n7 8 9"
   end
 
   test "returns message for draw" do
-    assert TicTacToe.UI.draw_message =~ "It's a draw!"
+    assert capture_io(fn ->
+      TicTacToe.UI.draw_message 
+    end) =~ "It's a draw!"
   end
 
   test "congratulates winner" do
-    assert TicTacToe.UI.congratulate_winner("Barry") =~ "Barry won! Congratulations!"
+    assert capture_io(fn ->
+      TicTacToe.UI.congratulate_winner("Barry") 
+    end) =~ "Barry won! Congratulations!"
   end
 end
