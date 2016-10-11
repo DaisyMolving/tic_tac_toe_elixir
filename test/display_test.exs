d7def7bc7f366fd9b16ceca665e647bd38fda65e
Daisy Molving <daisymolving@gmail.com>
2016-10-11 13:45:31 +0100

    modifies game tests that they test for game exit when player chooses not to play again, modifies tests for display that now have more spacing

--- test/display_test.exs
+++ test/display_test.exs
@@ -5,19 +5,19 @@ defmodule TicTacToeTest.Display do
   test "asks player to set name and continuously prints invalid message until name is valid" do
     assert capture_io("123\n!@£\ndaisy", fn ->
       TicTacToe.Display.request_to_validate(:name, "Player 1")
-    end) =~ "Player 1, please input your name: That is an invalid name.\n Please try again without using any non-letters: That is an invalid name.\n Please try again without using any non-letters: "
+    end) =~ "Player 1, please input your name: \n\nThat is an invalid name.\n Please try again without using any non-letters: \nThat is an invalid name.\n Please try again without using any non-letters: "
   end
 
   test "asks player to set marker and continuously prints invalid message until marker is valid" do
     assert capture_io("1\n$w\nx", fn ->
       TicTacToe.Display.request_to_validate(:marker, "Gary")
-    end) =~ "Gary, please choose a marker of any single character that is not a number. That is an invalid marker.\n Choose a single character that is not a number: That is an invalid marker.\n Choose a single character that is not a number: "
+    end) =~ "Gary, please choose a marker of any single character that is not a number: \n\nThat is an invalid marker.\n Choose a single character that is not a number: \nThat is an invalid marker.\n Choose a single character that is not a number: "
   end
 
   test "asks player to take turn and continuously prints invalid message until turn is valid" do
     assert capture_io("x\n$w\n1", fn ->
       TicTacToe.Display.request_to_validate(:turn, "Gary")
-    end) =~ "Gary's turn, input the number of the position that you would like to mark: That is not a valid turn.\n Please input a number corresponding to an unmarked space on the board: That is not a valid turn.\n Please input a number corresponding to an unmarked space on the board: "
+    end) =~ "Gary's turn, input the number of the position that you would like to mark: \n\nThat is not a valid turn.\n Please input a number corresponding to an unmarked space on the board: \nThat is not a valid turn.\n Please input a number corresponding to an unmarked space on the board: "
   end
 
   test "displays current board" do
