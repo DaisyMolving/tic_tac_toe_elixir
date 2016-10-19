279c787657c5f4dc739a4e320475fc47febf1c69
Daisy Molving <daisymolving@gmail.com>
2016-10-19 15:31:50 +0100

    welcomes and builds players

--- lib/game.ex
+++ lib/game.ex
@@ -1,4 +1,29 @@
 defmodule TicTacToe.Game do
-  alias TicTacToe.{Board, Player, Validator, CliDisplay, Messager}
+  alias TicTacToe.{Player, Validator, CliDisplay, Messager}
+
+  def play_tic_tac_toe do
+    welcome_players
+    {player_1, player_2} = build_players
+  end
+
+  def welcome_players do
+    CliDisplay.write(Messager.welcome_introduction)
+  end
+
+  def build_players do
+    player_1 = 
+      "Player 1" 
+      |> Messager.name_input_request 
+      |> CliDisplay.get_stripped_input 
+      |> Validator.validate_input(:name) 
+      |> Player.build("x")
+    player_2 = 
+      "Player 2" 
+      |> Messager.name_input_request 
+      |> CliDisplay.get_stripped_input 
+      |> Validator.validate_input(:name) 
+      |> Player.build("o")
+    {player_1, player_2}
+  end
 
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -1,4 +1,16 @@
 defmodule TicTacToeTest.Game do
   use ExUnit.Case
+  import ExUnit.CaptureIO
 
+  test "game greets players" do
+    assert capture_io(fn ->
+      TicTacToe.Game.welcome_players
+    end) =~ "Welcome to Tic Tac Toe"
+  end
+
+  test "builds players" do
+    assert capture_io("Barry\nGary", fn ->
+      TicTacToe.Game.build_players 
+    end) == ""
+  end
 end
