14f3e5a0769f8123524e787c95616e414ce5b314
Daisy Molving <daisymolving@gmail.com>
2016-10-25 14:23:57 +0100

    removes random number selection from computer player

--- lib/computer_player.ex
+++ lib/computer_player.ex
@@ -2,10 +2,6 @@ defmodule TicTacToe.ComputerPlayer do
 
   defstruct name: "Computer", marker: "o"
 
-  def choose_random_number do
-    Integer.to_string(Enum.random(1..9))
-  end
-
   def return_possible_boards(current_board, marker) do
     return_possible_boards(current_board, current_board, marker, [])
   end
--- test/computer_player_test.exs
+++ test/computer_player_test.exs
@@ -7,11 +7,6 @@ defmodule TicTacToeTest.ComputerPlayer do
     assert new_player.marker == "o"
   end
 
-  test "computer player chooses a random number" do
-    list_of_numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    assert Enum.member?(list_of_numbers, TicTacToe.ComputerPlayer.choose_random_number)
-  end
-
   test "current player returns possible moves for next turn on one board for each move" do
     current_board = ["x", "o", "o", "o", "x", "6", "7", "8", "9"]
     assert TicTacToe.ComputerPlayer.return_possible_boards(current_board, "x") == [
