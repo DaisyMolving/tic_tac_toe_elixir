8f552287b9b1fe105968fa1f85072f9ddd6942ff
Daisy Molving <daisymolving@gmail.com>
2016-11-08 16:08:27 +0000

    gives only or maximum terminal value

--- lib/minimax.ex
+++ lib/minimax.ex
@@ -1,20 +1,28 @@
 defmodule TicTacToe.Minimax do
 
   def best_move(current_board, {max_player, min_player}) do
-    elem(minimax(current_board, [], {max_player, min_player}), 1)
+    elem(minimax(current_board, {max_player, min_player}), 1)
   end
 
-  def minimax(current_board, minimax_values, {max_player, min_player}) do
+  def minimax(current_board, {max_player, min_player}) do
     cond do
       terminal?(find_values(current_board, max_player)) ->
-        {0, "3"}
+        give_terminal_value(find_values(current_board, max_player))
     end
   end
 
   def terminal?(minimax_values) do
-    Enum.count(minimax_values) == 1 or Enum.any?(minimax_values, fn({value, cell_number}) ->
+    Enum.count(minimax_values) == 1 or Enum.any?(minimax_values, fn({value, _}) ->
       value == 1
     end)
+  end
+
+  def give_terminal_value(minimax_values) do
+    if Enum.count(minimax_values) == 1 do
+      Enum.at(minimax_values, 0)
+    else
+      Enum.max(minimax_values)
+    end
   end
 
   def find_values(current_board, max_player) do
@@ -25,7 +33,6 @@ defmodule TicTacToe.Minimax do
         {0, changed_cell(current_board, possible_board)} 
       end
     end)
-    # for every available cell on the board return a new board with that cell marked, and determine whether a win for max_player or draw and return these tuples of minimax value and cell number in a list.
   end
 
   def possible_moves(current_board, max_player) do
@@ -52,7 +59,7 @@ defmodule TicTacToe.Minimax do
     Enum.find(Enum.zip(current_board, marked_board), fn({x, y}) ->
       x != y
     end)
-  |> elem(0)
+    |> elem(0)
   end
     
 end
--- test/minimax_test.exs
+++ test/minimax_test.exs
@@ -21,13 +21,31 @@ defmodule TicTacToeTest.Minimax do
     assert TicTacToe.Minimax.find_values(current_board, max_player) == [{1, "1"}, {0, "3"}, {0, "5"}]
   end
 
-  # test "computer marks only remaining available space" do
-  #   current_board = ["x", "o", "3",
-  #                    "o", "x", "x",
-  #                    "o", "x", "o"]
-  #   player_1 = TicTacToe.HumanPlayer.build("Computer", "o")
-  #   player_2 = TicTacToe.HumanPlayer.build("gary", "x")
-  #   assert TicTacToe.Minimax.best_move(current_board, {player_1, player_2}) == "3"
-  # end
+  test "gives a draw terminal value" do
+    current_board = ["x", "o", "3",
+                     "o", "x", "x",
+                     "o", "x", "o"]
+    max_player = TicTacToe.HumanPlayer.build("Computer", "o")
+    values = TicTacToe.Minimax.find_values(current_board, max_player)
+    assert TicTacToe.Minimax.give_terminal_value(values) == {0, "3"}
+  end
+
+  test "gives a win terminal value" do
+    current_board = ["x", "o", "o",
+                     "4", "5", "x",
+                     "o", "8", "x"]
+    max_player = TicTacToe.HumanPlayer.build("Computer", "o")
+    values = TicTacToe.Minimax.find_values(current_board, max_player)
+    assert TicTacToe.Minimax.give_terminal_value(values) == {1, "5"}
+  end
+
+  test "computer marks only remaining available space" do
+    current_board = ["x", "o", "3",
+                     "o", "x", "x",
+                     "o", "x", "o"]
+    player_1 = TicTacToe.HumanPlayer.build("Computer", "o")
+    player_2 = TicTacToe.HumanPlayer.build("gary", "x")
+    assert TicTacToe.Minimax.best_move(current_board, {player_1, player_2}) == "3"
+  end
 
 end
