17ed0d47ccfb44f0c6f5b3d41e8b85189b0ac49f
Daisy Molving <daisymolving@gmail.com>
2016-11-15 12:02:03 +0000

    returns best terminal move when win, by finding max of returned values of current board cells

--- lib/minimax.ex
+++ lib/minimax.ex
@@ -1,26 +1,64 @@
 defmodule TicTacToe.Minimax do
 
-  def best_move(current_board) do
-    minimax(current_board)
+  def best_move(current_board, current_marker) do
+    minimax(current_board, current_marker)
   end
 
-  def minimax(current_board) do
+  def minimax(current_board, current_marker) do
     cond do
-      terminal?(current_board) ->
-        terminal_cell(current_board)
+      terminal?(current_board, current_marker) ->
+        terminal_cell(current_board, current_marker)
     end
   end
 
-  def terminal?(current_board) do
+  def terminal?(current_board, current_marker) do
+    terminal_draw(current_board) or terminal_win(current_board, current_marker)
+  end
+
+  def terminal_draw(current_board) do
     current_board
     |> available_cells
     |> Enum.count == 1
   end
 
-  def terminal_cell(current_board) do
+  def terminal_win(current_board, current_marker) do
+    values(possible_boards(current_board, current_marker), current_board)
+  end
+
+  def values(possible_boards, current_board) do
+    Enum.map(possible_boards, fn(possible_board) ->
+      if TicTacToe.Board.win?(possible_board) do
+        {1, changed_cell(current_board, possible_board)}
+      else
+        {0, changed_cell(current_board, possible_board)}
+      end
+    end)
+  end
+
+  def possible_boards(current_board, current_marker) do
+    Enum.map(available_cells(current_board), fn(cell) ->
+      ghost_mark(cell, current_marker, current_board)
+    end)
+  end
+
+  def changed_cell(current_board, possible_board) do
+    comparisons = Enum.zip(current_board, possible_board)
+    Enum.find(comparisons, fn({current_cell, possible_cell}) ->
+      current_cell != possible_cell
+    end)
+    |> elem(0)
+  end
+
+  def ghost_mark(cell, current_marker, current_board) do
+    List.replace_at(current_board, String.to_integer(cell) - 1, current_marker)
+  end
+
+  def terminal_cell(current_board, current_marker) do
     current_board
-    |> available_cells
+    |> possible_boards(current_marker)
+    |> values(current_board)
     |> Enum.max
+    |> elem(1)
   end
     
   def available_cells(current_board) do
